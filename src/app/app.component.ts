f0dd14dca7cd0eda27f1268802df3aee04aad9ae
codeByCandlelight <Terryltilley@gmail.com>
2021-11-12 16:11:19 -0800

    Finished assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isUsernameFilled = false;
+  username = '';
+
+  // onUpdateInputField(event: any) {
+  // this.username = (<HTMLInputElement>event.target).value;
+  //   this.isUsernameBlank = true;
+  //   console.log(event);
+  // }
+
+  onClearUsername() {
+    this.isUsernameFilled = false;
+    this.username = '';
+  }
 }
